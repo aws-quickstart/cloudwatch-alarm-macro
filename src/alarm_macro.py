8e329f223ed05abc2b197a216ea009155480d6dd
AlexNicot <anicot@amazon.co.uk>
2019-10-29 13:48:43 +0000

    adding back alarms

--- src/alarm_macro.py
+++ src/alarm_macro.py
@@ -4,7 +4,7 @@
 import os
 import logging
 
-Monitoring_Topic = os.environ['Monitoring_Topic']
+Monitoring_Topic = os.environ['SNSTopic']
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,6 +14,32 @@ cpu = {'AlarmName':'CPUUtilization','MetricName':'CPUUtilization','EvaluationPer
 
 statuscheck = {'AlarmName':'StatusCheck','MetricName':'StatusCheckFailed_Instance', 'EvaluationPeriods': '5', 'ComparisonOperator': 'GreaterThanOrEqualToThreshold', 'DimensionName': 'InstanceId',
               'Namespace': 'AWS/EC2', 'Period': '120', 'Statistic': 'Average', 'Threshold': '85', 'Unit': 'Percent'}
+
+alb_5xx_count = {'AlarmName':'5xxErrors','MetricName':'HTTPCode_ELB_5XX_Count', 'EvaluationPeriods': '5', 'ComparisonOperator': 'GreaterThanOrEqualToThreshold', 'DimensionName': 'LoadBalancer',
+              'Namespace': 'AWS/ApplicationELB', 'Period': '60', 'Statistic': 'Sum', 'Threshold': '0', 'Unit': 'Count'}
+
+alb_rejected_request_count = {'AlarmName':'RejectedRequests','MetricName':'RejectedConnectionCount', 'EvaluationPeriods': '5', 'ComparisonOperator': 'GreaterThanOrEqualToThreshold', 'DimensionName': 'LoadBalancer',
+              'Namespace': 'AWS/ApplicationELB', 'Period': '120', 'Statistic': 'Sum', 'Threshold': '1', 'Unit': 'Count'}
+
+nlb_UnHealthyHostCount = {'AlarmName':'UnHealthyHostCount','MetricName':'UnHealthyHostCount', 'EvaluationPeriods': '1', 'ComparisonOperator': 'GreaterThanOrEqualToThreshold', 'DimensionName': 'LoadBalancer',
+              'Namespace': 'AWS/NetworkELB', 'Period': '120', 'Statistic': 'Minimum', 'Threshold': '0', 'Unit': 'Count'}
+
+nlb_HealthyHostCount = {'AlarmName':'HealthyHostCount','MetricName':'HealthyHostCount', 'EvaluationPeriods': '1', 'ComparisonOperator': 'LessThanThreshold', 'DimensionName': 'LoadBalancer',
+              'Namespace': 'AWS/NetworkELB', 'Period': '120', 'Statistic': 'Minimum', 'Threshold': '1', 'Unit': 'Count'}
+
+lambda_4xx_count = {'AlarmName':'4xxErrors','MetricName':'Errors', 'EvaluationPeriods': '1', 'ComparisonOperator': 'GreaterThanOrEqualToThreshold', 'DimensionName': 'FunctionName',
+                    'Namespace': 'AWS/Lambda', 'Period': '60', 'Statistic': 'Sum', 'Threshold': '5', 'Unit': 'Count'}
+
+lambda_invocations_count = {'AlarmName':'Invocations','MetricName':'Invocations', 'EvaluationPeriods': '1', 'ComparisonOperator': 'GreaterThanOrEqualToThreshold', 'DimensionName': 'FunctionName',
+                    'Namespace': 'AWS/Lambda', 'Period': '300', 'Statistic': 'Sum', 'Threshold': '20', 'Unit': 'Count'}
+
+nat_ErrorPortAllocation = {'AlarmName':'ErrorPortAllocations','MetricName':'ErrorPortAllocation', 'EvaluationPeriods': '1', 'ComparisonOperator': 'GreaterThanThreshold', 'DimensionName': 'NatGatewayId',
+                    'Namespace': 'AWS/NATGateway', 'Period': '300', 'Statistic': 'Sum', 'Threshold': '0', 'Unit': 'Count'}
+
+nat_ActiveConnectionCount = {'AlarmName':'ActiveConnections','MetricName':'ActiveConnectionCount', 'EvaluationPeriods': '1', 'ComparisonOperator': 'GreaterThanThreshold', 'DimensionName': 'NatGatewayId',
+                    'Namespace': 'AWS/NATGateway', 'Period': '300', 'Statistic': 'Maximum', 'Threshold': '100', 'Unit': 'Count'}
+
+
 
 def handler(event, context):
     alarm_dictionary = {}
@@ -25,11 +51,32 @@ def handler(event, context):
         resource_json = resources[resource]
         try:
             if resource_json['Type'] == 'AWS::EC2::Instance':
+                logger.info('Resource {} is an EC2'.format(resource))
                 condition = condition_checker(resource,resource_json)
                 alarms = [cpu, statuscheck]
                 generate_alarm(resource,Monitoring_Topic,alarms,alarm_dictionary,condition)
+            elif resource_json['Type'] == 'AWS::ElasticLoadBalancingV2::LoadBalancer':
+                condition = condition_checker(resource, resource_json)
+                lb_type = resource_json['Properties']['Type'].lower()
+                if lb_type == 'application':
+                    logger.info('Resource {} is an ALB'.format(resource))
+                    alarms = [alb_rejected_request_count, alb_5xx_count]
+                elif lb_type == 'network':
+                    logger.info('Resource {} is an NLB'.format(resource))
+                    alarms = [nlb_HealthyHostCount, nlb_UnHealthyHostCount]
+                generate_alarm(resource, Monitoring_Topic, alarms, alarm_dictionary, condition)
+            elif resource_json['Type'] == 'AWS::EC2::NatGateway':
+                logger.info('Resource {} is a NAT Gateway'.format(resource))
+                condition = condition_checker(resource, resource_json)
+                alarms = [nat_ErrorPortAllocation, nat_ActiveConnectionCount]
+                generate_alarm(resource, Monitoring_Topic, alarms, alarm_dictionary, condition)
+            elif resource_json['Type'] == 'AWS::Lambda::Function':
+                logger.info('Resource {} is a lambda function'.format(resource))
+                condition = condition_checker(resource, resource_json)
+                alarms = [lambda_4xx_count, lambda_invocations_count]
+                generate_alarm(resource, Monitoring_Topic, alarms, alarm_dictionary, condition)
             else:
-                logger.info('Resource {} is not a supported type'.format(resource))
+                logger.info('Resource {} is not of a supported resource type'.format(resource))
         except Exception as e:
             logger.error('ERROR {}'.format(e))
             resp = {
@@ -57,14 +104,13 @@ def condition_checker(resource, resource_json):
     if 'Condition' in resource_json:
         print('Condition found for {}'.format(resource))
         condition = resource_json['Condition']
+        logger.info(f'{resource} condition is {condition}')
     else:
         print('No Condition Statement found for {}..'.format(resource))
         condition = 'none'
-    logger.info(f'{resource} condition is {condition}')
     return condition
 
 def generate_alarm(resource,Monitoring_Topic,alarms,alarm_dictionary,condition):
-
     for alarm in alarms:
         alarm_json = {f'{resource}{alarm["AlarmName"]}':{
                     "Type": "AWS::CloudWatch::Alarm",
